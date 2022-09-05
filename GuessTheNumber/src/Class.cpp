bf82b84d5859bb2738890b8b04f56effa2536751
Sweipe <aled@hotmail.se>
2022-09-05 13:13:58 +0200

    Final
    It works ithkn

--- GuessTheNumber/src/Class.cpp
+++ GuessTheNumber/src/Class.cpp
@@ -4,50 +4,49 @@
 #include <Windows.h>
 
 Game::Game() {
-	guess_count = 4;
+	guess_count = 10;
 	attempts = 0;
+	srand((unsigned)time(0));
 	number = rand() % 100 + 1;
 	win_count = 0;
 }
 
 void Game::Run() {
 
-
+	int exit_flag = 1;
 	int guess = 0;
 	std::string input;
 
 	std::cout << "Welcome to 'Number Guesser'!\nInput a number between 1 and 100.\n" <<
-	"You have 4 attempts remaining.\nYour guess: ";
+	"You have "<<guess_count<<" attempts remaining.\nYour guess: ";
+	while (exit_flag == 1) {
+		while (true) {
 
-	while (true) {
+			std::cin >> guess;
 
-		std::cin >> input;
-		std::cout << typeid(input).name();
-
-		if (guess < 1 || guess>100) {
-			std::cout << guess;
-			//std::cout << "Really?\nGuess again: ";
-		}
-		else if (guess < number) {
-			attempts--;
-			std::cout << "Greater >\n";
-		}
-		else if (guess > number) {
-			attempts--;
-			std::cout << "Lesser <\n";
-		}
-		else {
-			std::cout << "Correct!";
-			break;
+			if (attempts >= guess_count) {
+				std::cout << "You have no attempts remaining, thus you've failed.\n";
+				exit_flag = 0;
+				break;
+			}
+			else if (guess < 1 || guess>100) {
+				std::cout << "Really?\nGuess again: ";
+			}
+			else if (guess < number) {
+				attempts++;
+				std::cout << "Greater >\n";
+			}
+			else if (guess > number) {
+				attempts++;
+				std::cout << "Lesser <\n";
+			}
+			else {
+				win_count++;
+				attempts = 0;
+				std::cout << "Correct!\nYour Current score is " << win_count <<
+				"\nYou have " << guess_count << " attempts remaining.\nYour guess: ";
+				number = rand() % 100 + 1;
+			}
 		}
 	}
-}
-
-void Game::Decrease() {
-	if (attempts <= 0) {return;}
-	attempts--;
-	
-	HANDLE outp = GetStdHandle(STD_OUTPUT_HANDLE);
-	SetConsoleCursorPosition(outp, { 9,3 });
-	WriteConsole(outp, "d", 1, NULL, NULL);
 }
