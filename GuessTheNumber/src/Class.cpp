6afe3590b58ad5302c1ecd4a1659fd9423173854
Sweipe <aled@hotmail.se>
2022-09-05 09:32:39 +0200

    ´+

--- GuessTheNumber/src/Class.cpp
+++ GuessTheNumber/src/Class.cpp
@@ -1,32 +1,39 @@
 #include "class.h"
 #include <iostream>
 #include <string>
+#include <Windows.h>
 
 Game::Game() {
+	guess_count = 4;
+	attempts = 0;
+	number = rand() % 100 + 1;
+	win_count = 0;
 }
 
 void Game::Run() {
-	int guess;
-	int guess_count;
-	int attempts;
-	int win_count;
 
-	int number = rand()%100+1;
 
-	std::cout << "Welcome to 'Number Guesser'!\nInput a number between 1 and 100.\nYour guess: ";
+	int guess = 0;
+	std::string input;
+
+	std::cout << "Welcome to 'Number Guesser'!\nInput a number between 1 and 100.\n" <<
+	"You have 4 attempts remaining.\nYour guess: ";
 
 	while (true) {
 
-		std::cin >> guess;
+		std::cin >> input;
+		std::cout << typeid(input).name();
 
 		if (guess < 1 || guess>100) {
-			std::cout << "Really?\nGuess again: ";
-			continue;
+			std::cout << guess;
+			//std::cout << "Really?\nGuess again: ";
 		}
 		else if (guess < number) {
+			attempts--;
 			std::cout << "Greater >\n";
 		}
 		else if (guess > number) {
+			attempts--;
 			std::cout << "Lesser <\n";
 		}
 		else {
@@ -34,4 +41,13 @@ void Game::Run() {
 			break;
 		}
 	}
+}
+
+void Game::Decrease() {
+	if (attempts <= 0) {return;}
+	attempts--;
+	
+	HANDLE outp = GetStdHandle(STD_OUTPUT_HANDLE);
+	SetConsoleCursorPosition(outp, { 9,3 });
+	WriteConsole(outp, "d", 1, NULL, NULL);
 }
--- GuessTheNumber/src/class.h
+++ GuessTheNumber/src/class.h
@@ -2,6 +2,12 @@
 
 class Game {
 public:
+	int guess_count;
+	int attempts;
+	int number;
+	int win_count;
+
 	Game();
 	void Run();
+	void Decrease();
 };
